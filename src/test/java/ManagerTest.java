5e52e8b63a298f4d8abf4ccc0383f023af99703d
Mark Richardson <mark.richardson4@gmail.com>
2021-02-01 16:26:49 +0000

    added methods and tests for manager (subclass of employee)

---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,46 @@
+import management.Manager;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ManagerTest {
+
+    Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("Michael Scott", "JT m4 d3 up C", 30000.00, "Sales");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Michael Scott", manager.getName());
+    }
+
+    @Test
+    public void hasNiNumber(){
+        assertEquals("JT m4 d3 up C", manager.getNiNumber());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(30000.00, manager.getSalary(), 0.1);
+    }
+
+    @Test
+    public void hasDeptName(){
+        assertEquals("Sales", manager.getDeptName());
+    }
+
+    @Test
+    public void canRaiseSalary(){
+        manager.raiseSalary(500.00);
+        assertEquals( 30500.00, manager.getSalary(), 0.0);
+    }
+
+    @Test
+    public void canPayBonus(){
+        assertEquals(300.00, manager.payBonus(), 0.0);
+    }
+}
